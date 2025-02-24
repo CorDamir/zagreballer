7c311c5548f89443a1fc5c886f6f96f28aae4376
CorDamir <cordamir@gmail.com>
2025-02-24 04:32:09 +0100

    fix deployment: uncomment STATIC_ROOT declaration

--- zagreballer/settings.py
+++ zagreballer/settings.py
@@ -20,7 +20,7 @@ if os.path.isfile('env.py'):
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
