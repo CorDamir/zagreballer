d53d52332152e17884505a303b398f07e189f0c6
CorDamir <cordamir@gmail.com>
2025-02-24 10:31:07 +0100

    simplify custom user registration to admin

--- user_accounts/admin.py
+++ user_accounts/admin.py
@@ -1,15 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .forms import PlayerCreationForm, PlayerChangeForm
 from .models import Player
 
 
 # Register your models here.
-class PlayerAdmin(UserAdmin):
-    add_form = PlayerCreationForm
-    form = PlayerChangeForm
-    model = Player
-    list_display = ["username", "email"]
-
-
-admin.site.register(Player, PlayerAdmin)
+admin.site.register(Player)
--- user_accounts/forms.py
+++ user_accounts/forms.py
@@ -2,19 +2,3 @@ from django import forms
 from .models import Player
 
 
-class PlayerCreationForm(forms.ModelForm):
-    class Meta:
-        model = Player
-        fields = ("username", "email")
-
-
-class PlayerChangeForm(forms.ModelForm):
-    class Meta:
-        model = Player
-        fields = ("username", "email")
-
-
-class LoginForm(forms.ModelForm):
-    class Meta:
-        model = Player
-        fields = ("username", "password")
