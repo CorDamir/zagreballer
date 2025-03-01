12b2544363f4cb833015c77de247dc00e371aec1
CorDamir <cordamir@gmail.com>
2025-03-01 10:12:30 +0100

    finalize game display styling, add collapsable menu functionality for mobile device screen sizes

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,17 +1,9 @@
+function closePopup(){ this.classList.toggle("hidden"); }
+function toggleMenu(e){ if (e.target == this) this.classList.toggle("open") }
+
 function docLoaded(){
-    userFormsBtn = document.getElementById("login-signup-link");
-
-    try {
-        userFormsBtn.addEventListener("click", e => {
-            document.getElementById("login-form-container").classList.toggle("hidden");
-        });
-    }
-    catch {} // when user is logged in "userFormsBtn" isn't in the document,
-             // this prevents error from stopping the script
-
-    document.getElementById("popup-close-button").addEventListener("click", e =>{
-        document.getElementById("popup-message").classList.toggle("hidden");
-    })
+    document.getElementById("popup-message").addEventListener("click", closePopup);
+    document.getElementById("menu-options").addEventListener("click", toggleMenu);
 }
 
 document.onload = docLoaded();
