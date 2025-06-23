cb171e6b8890e7bbf7ba3ac44da8381e50ec8273
CorDamir <cordamir@gmail.com>
2025-06-23 15:20:40 +0200

    add: basic structure for password reset functionality using django's built-in mechanism

--- user_accounts/urls.py
+++ user_accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login', views.login_handler, name="login_handler"),
@@ -11,4 +12,32 @@ urlpatterns = [
         views.show_personal_profile,
         name="my_profile"
         ),
+    path(
+        "forgotten-password/",
+        auth_views.PasswordResetView.as_view(
+            template_name="forgotten-password/reset_form.html"
+        ),
+        name="reset_form",
+    ),
+    path(
+        "forgotten-password/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="forgotten-password/reset_done.html"
+        ),
+        name="reset_done",
+    ),
+    path(
+        "forgotten-password/confirm/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="forgotten-password/reset_confirm.html"
+        ),
+        name="reset_confirm",
+    ),
+    path(
+        "forgotten-password/complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="forgotten-password/reset_complete.html"
+        ),
+        name="reset_complete",
+    ),
     ]
